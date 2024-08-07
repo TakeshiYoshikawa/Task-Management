df1ec4b933a9d3d1b29e2580352acca80e50414f
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-07 20:31:47 -0300

    Adjust TODO header content position

--- todo/src/components/Header.jsx
+++ todo/src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return (
-        <header>
+        <header className="todo_header">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
