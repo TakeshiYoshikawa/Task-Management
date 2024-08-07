3f5b8593e5379aa69c00c0eb9da66b67a3c4b396
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-07 20:26:40 -0300

    Set up front end

--- todo/src/components/Header.jsx
+++ todo/src/components/Header.jsx
@@ -1,11 +1,24 @@
 function Header(){
-    return(
-        <>
-            <svg>
-                <path d=""/>
+    return (
+        <header>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width={40}
+                height={40}
+                fill="#fff"
+                strokeLinecap="round"
+                stroke="#000000"
+                strokeWidth="2"
+            >
+                <path 
+                    d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"
+                    fillRule="nonzero"
+                />
+                <polyline points="22,6 12,13 2,6"/>
             </svg>
             <h1>TODO</h1>
-        </>
+        </header>
     );
 }
 
