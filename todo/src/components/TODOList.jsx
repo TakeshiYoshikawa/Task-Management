2b0b3b791e26f8c618eb0ae0d93a0e1605691580
TakeshiYoshikawa <takeshiy096@gmail.com>
2024-08-07 23:44:50 -0300

    Add hover effect on Task item

--- todo/src/components/TODOList.jsx
+++ todo/src/components/TODOList.jsx
@@ -12,9 +12,34 @@ function TODOList({todos, setTodos}) {
 }
 
 function Item({item, todos, setTodos}){
+    const completeTodo = () => {
+        setTodos((previousTodos) =>
+            previousTodos.map((todo) =>
+                todo.id === item.id? {...todo, is_completed: !todo.is_completed} : todo
+            )
+        );
+    }
+
     return(
-        <li id="item?.id" className="todo_item">
+        <li id="item?.id" className="todo_item" onClick={completeTodo}>
+            <button className="todo_items_left">
+                <svg
+                clipRule="evenodd"
+                fillRule="evenodd"
+                strokeLinejoin="round"
+                strokeMiterlimit="2"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                width={34}
+                height={34}
+                stroke="#22C55E"
+                fill={item.is_completed ? "#22C55E" : "#0d0d0d"}
+                >
+                    <circle cx={11.998} cy={11.998} fillRule="nonzero" r={9.998}/>
+                </svg>
             <p>{item?.title}</p>
+            </button>            
+
             <form className="edit-form">
             </form>
             <div className="todo_items_right">
